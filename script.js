c284e32e8395c71145bf26a0519f90b99eba003e
s4gor <imrans4gor@gmail.com>
2021-01-28 15:15:24 +0600

    fixed issues and added some features

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
-val=Math.floor(Math.random()*11);
-correct_ans=val;
-console.log(correct_ans);
-var x;
-var flag=0;
+"use strict";
 
-for(var i=1;i<=3;i++)
-{
-	x=prompt("Enter a number :");
+var playAgain = document.getElementById('playagain');
+playAgain.style.display = "none";
+var win = document.getElementById('right');
+var loss = document.getElementById("wrong");
 
-	if(correct_ans<x)
-	{
-		alert("Correct answer is smaller!!");
+function guessTheNumber() {
+	var answer = Math.floor(Math.random()*11);
+
+	for(var i = 1; i <= 3; i++) {
+		var guessedNumber = Number(prompt("Guess any number from 0 - 10\nYou will get 3 chance to win the game\nEnter a number :"));
+
+		if(guessedNumber != '' && typeof guessedNumber == "number") {
+			if(guessedNumber > answer) {
+				alert('Correct answer is smaller!!');
+			} else if(guessedNumber < answer) {
+				alert('Correct answer is greater!!');
+			} else {
+				win.style.display = "block";
+				win.innerHTML = "You win :)";
+				playAgain.style.display = "block";
+				break;
+			}
+		} else {
+			alert('Put a number');
+		}
+
+		if(i == 3) {
+			loss.style.display = "block";
+			loss.innerHTML="You loss:(";
+			playAgain.style.display = "block";
+			
+		}
 	}
-	else if(correct_ans>x)
-	{
-		alert("Correct answer is greater!!");
-	}
-	else
-	{
-		flag=1;
-		break;
-	}
+
 }
-if(flag==1)
-{
-	document.getElementById("demo").innerHTML="You win :)";
-}
-else
-{
-	document.getElementById("demo1").innerHTML="You loss:(";
-}
+
+guessTheNumber();
+
+playAgain.addEventListener('click', () => {
+	playAgain.style.display = 'none';
+	win.style.display = 'none';
+	loss.style.display = 'none';
+	setTimeout(guessTheNumber, 30);
+});
